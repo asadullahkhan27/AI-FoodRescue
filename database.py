f0267c700fa1a5e55f8371e30d87caaf632459fd
Asad Ullah Khan <asadullahkhan231569@gmail.com>
2026-09-12 12:45:47 +0500

    Create database.py

---
+++ database.py
@@ -0,0 +1,10 @@
+{
+  "food_type": "rice",
+  "condition": "deteriorating",
+  "confidence": 91.4,
+  "storage_type": "Refrigerated",
+  "duration_hours": 18,
+  "temperature": 4,
+  "decision": "USE SOON",
+  "timestamp": "2026-09-12T12:40:00"
+}
